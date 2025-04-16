3424a156aa4adef13b880903ebce5783f226459b
Dominik Peters <mail@dominik-peters.de>
2025-04-16 23:40:23 +0900

    encode emails in utf-8

--- get_oyez.py
+++ get_oyez.py
@@ -38,7 +38,7 @@ Subject: SCOTUS: {subject}
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(user, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, message.encode('utf-8'))
 
 import logging
 log = logging.getLogger(__name__)
